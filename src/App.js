3d58d9a8de9bab8fd1244df86dc1fd1e25ddc831
Arif Hossan <arif.hossan.eu@gmail.com>
2023-08-07 01:11:56 +0600

    using useEffect load data from a restAPI and display with style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoadPost></LoadPost>
       <District name='Cumilla' special='Bus Stop'></District>
       <District name='Sylhet' special='Nature'></District>
       <District name='Chittagong' special='Hilly Area'></District>
@@ -13,7 +14,7 @@ function App() {
 //css style for each district
 const district = {
   backgroundColor:'lightGrey',
-  padding:'5px',
+  padding:'15px',
   margin:'5px',
   borderRadius:'10px'
 }
@@ -27,6 +28,32 @@ function District (props) {
       <p>Special : {props.special}</p>
       <h6>Power : {power}</h6>
       <button onClick={boostPower}>Boost Power</button>
+    </div>
+  )
+}
+//Post Component
+function LoadPost () {
+  const [posts,setPost]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data =>setPost(data))
+  },[])
+  return (
+    <div>
+      <h1>Post : {posts.length}</h1>
+      {
+        posts.map((post,idx) => <Post key={idx} title={post.title} body={post.body}></Post>)
+      }
+    </div>
+  )
+}
+//single Post Component
+const Post= (props)=>{
+  return (
+    <div style={{backgroundColor:'lightsalmon', padding:'5px',border:'2px solid gray', margin:'2px'}}>
+      <h6>Title : {props.title}</h6>
+      <p>Body : {props.body}</p>
     </div>
   )
 }
